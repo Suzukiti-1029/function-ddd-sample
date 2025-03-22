6b5cdb429600e115921f6879cc617bbf1cc29c55
Suzukiti-1029 <h40396suzujun@gmail.com>
2025-03-22 22:03:34 +0000

    feat: 検証ステップの実装；エフェクトを排除し、実装（実装途中のためエラー発生中）

---
+++ OrderTaking/PlaceOrderWorkflow.work.fs
@@ -0,0 +1,161 @@
+namespace OrderTaking.PlaceOrderWorkflowWork
+
+// ! PlaceOrderWorkflowの型定義のエフェクトを排除したものを実装している
+
+open OrderTaking.Domain
+// ドメインAPIモジュールから型を持ってくる
+open OrderTaking.DomainApi
+open OrderTaking.PlaceOrderWorkflow
+
+// // * Domain.ValueObject
+// // 製品コード関連
+// type WidgetCode = WidgetCode of string
+//   // 制約: 先頭が"W"＋数字4桁
+// type GizmoCode = GizmoCode of string
+//   // 制約: 先頭が"G"＋数字3桁
+// type ProductCode =
+//   | Widget of WidgetCode
+//   | Gizmo of GizmoCode
+// type Price = Undefined
+
+// --------------------
+// 注文のライフサイクル
+// --------------------
+
+// 検証済みの状態
+// // * Domain.ValueObject
+// type ValidatedOrderLine = Undefined
+
+// type CustomerInfo = Undefined
+
+// // * Domain.Entity
+// type ValidatedOrder = {
+//   OrderID: ValueObject.OrderID
+//   CustomerInfo: CustomerInfo
+//   ShippingAddress: Address
+//   BillingAddress: Address
+//   OrderLines: ValidatedOrderLine list
+// }
+
+// 価格計算済みの状態
+// // * Domain.ValueObject
+// type PricedOrderLine = Undefined
+
+// // * Domain.Entity
+// type PricedOrder = {
+//   OrderID: ValueObject.OrderID // エンティティのID
+//   CustomerInfo: CustomerInfo
+//   ShippingAddress: Address
+//   BillingAddress: Address
+//   // 検証済の注文明細行とは異なる
+//   OrderLines: PricedOrderLine list
+//   AmountToBill: BillingAmount
+// }
+
+// 全状態の結合
+// // * Domain.Entity(Aggregate)
+// type Order =
+//   | UnValidated of UnValidatedOrder
+//   | Validated of ValidatedOrder
+//   | Priced of PricedOrder
+//   // etc...
+
+// --------------------
+// 内部ステップの定義
+// --------------------
+
+// ----- 注文の検証 -----
+
+// 注文の検証が使用するサービス
+// // * Domain.Interface.Provider <|.. Infrastructure.Provider
+// // エラーを返す可能性がなく、リモート呼び出しでもない（自律性）
+// // （ローカルなキャッシュコピーが利用可能） としている
+// type CheckProductCodeExists = ProductCode -> bool
+
+// // * Domain.Errors
+// type AddressValidationError = AddressValidationError of string
+
+// // * Domain.ValueObject
+// type CheckedAddress = CheckedAddress of Undefined
+
+// * Domain.Interface.Remote <|.. Infrastructure.Remote
+// リモートのサービスを呼び出している
+type CheckAddressExists =
+  UnValidatedAddress -> CheckedAddress
+
+// * Usecases.Workflows
+type ValidateOrder =
+  CheckProductCodeExists // 依存関係
+    -> CheckAddressExists // 依存関係
+    -> UnValidatedOrder // 入力
+    -> ValidatedOrder
+
+// ----- 注文の価格計算 -----
+
+// 注文の価格計算が使用するサービス
+// // * Domain.Interface.Provider <|.. Infrastructure.Provider
+// type GetProductPrice = ProductCode -> Price
+
+// // * Domain.Errors
+// type PricingError = PricingError of string
+
+// // * Usecases.Workflows
+// // サブステップ自体にエラーが発生する可能性がある（結果が馬鹿でかい値やマイナス値など）
+// type PriceOrder =
+//   GetProductPrice // 依存関係
+//     -> ValidateOrder // 入力
+//     -> Result<PricedOrder, PricingError> // 出力
+
+// --------------------
+// 実装
+// --------------------
+// TODO 未整理モジュール
+module InComplete =
+  let toCustomerInfo (customer: UnValidatedCustomer) : CustomerInfo =
+    // 顧客情報の各種プロパティを作成する
+    // 無効な場合は例外を投げる
+    let firstName = customer.FirstName |> String50.create
+    let lastName = customer.LastName |> String50.create
+    let emailAddress = customer.EmailAddress |> EmailAddress.create
+
+    // 個人名を作成する
+    let name: PersonalName = {
+      FirstName = firstName
+      LastName = lastName
+    }
+
+    // 顧客情報を作成する
+    let customerInfo: CustomerInfo = {
+      Name = name
+      EmailAddress = emailAddress
+    }
+    // そしてそれを返す
+    customerInfo
+
+module Workflows =
+  let validateOrder: ValidateOrder =
+    fun checkProductCodeExists checkAddressExists unValidatedOrder ->
+
+    let orderID =
+      unValidatedOrder.OrderID
+      |> ValueObject.OrderID.create
+
+    let customerInfo =
+      unValidatedOrder.CustomerInfo
+      |> InComplete.toCustomerInfo // ヘルパー関数
+
+    let shippingAddress =
+      unValidatedOrder.ShippingAddress
+      |> toAddress // TODO ヘルパー関数
+
+    // TODO unValidatedOrder の各プロパティに対して同様に行う
+
+    // すべてのフィールドの準備ができたら、それらを使って
+    // 新しい「検証済みの注文」レコードを作成し、返す
+    {
+      OrderID = orderID
+      CustomerInfo = customerInfo
+      // ShippingAddress = shippingAddress
+      // BillingAddress = // TODO ...
+      // Lines = // TODO ...
+    }
