0348c9a2f870f62b78d7a45f197a003aacb0760f
Suzukiti-1029 <h40396suzujun@gmail.com>
2025-03-30 12:36:38 +0000

    chore: 依存関係のテスト；製品が存在しない場合の検証失敗を確認

--- tests/TestPlaceOrderWorkflow.work.fs
+++ tests/TestPlaceOrderWorkflow.work.fs
@@ -62,3 +62,56 @@ type DomainApiTests () =
 
     let resultOrderID= ValueObject.OrderID.value result.OrderID
     Assert.That(resultOrderID, Is.EqualTo "固定OrderID")
+
+  [<Test>]
+  member _.``製品が存在しない場合は、検証に失敗する``() =
+    // 準備: サービス依存関係のスタブを設定
+    let checkAddressExists: CheckAddressExists =
+      fun _ ->
+        CheckAddress {
+          AddressLine1 = "千代田区"
+          AddressLine2 = "千代田"
+          AddressLine3 = "1-1"
+          AddressLine4 = "皇城"
+          City = "東京都"
+          ZipCode = "100-8111"
+        } // 成功
+
+    let checkProductCodeExists: CheckProductCodeExists =
+      fun _ ->
+        false // 失敗
+
+    // 準備: 入力の設定
+    let unValidateOrder: UnValidatedOrder = {
+      OrderID = "固定OrderID"
+      CustomerInfo = {
+        FirstName = "固定FirstName"
+        LastName = "固定LastName"
+        EmailAddress = "sample@test.com"
+      }
+      ShippingAddress = UnValidateAddress "東京都千代田区千代田1-1 皇城"
+      BillingAddress = UnValidateAddress "東京都千代田区千代田1-1 皇城"
+      OrderLines = [
+        {
+          OrderLineID = "OI12345678"
+          ProductCode = "W1234"
+          Quantity = 1.0m<Data.UnitSystems.SI.UnitSymbols.kg>
+        }
+        {
+          OrderLineID = "OI23456789"
+          ProductCode = "G123"
+          Quantity = 2.0m<Data.UnitSystems.SI.UnitSymbols.kg>
+        }
+      ]
+    }
+
+    // TODO 現状、例外が発生するので、例外をキャッチして、Assertする
+    // 実行: validateOrderを呼び出す
+    // 検証: 結果が失敗であることを確認する
+    Assert.Throws<System.Exception>(fun () ->
+      Workflows.validateOrder
+        checkProductCodeExists
+        checkAddressExists
+        unValidateOrder
+        |> ignore
+    ) |> ignore
